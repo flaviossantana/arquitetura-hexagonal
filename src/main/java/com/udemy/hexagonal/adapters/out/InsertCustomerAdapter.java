f87355fd7da0b63503e81c1b44408456c7335a21
flavio.santana <9090flavio@gmail.com>
2026-03-13 19:31:30 -0300

    hexagonal: adicionados mapper e adapter para inserção de cliente

--- src/main/java/com/udemy/hexagonal/adapters/out/InsertCustomerAdapter.java
+++ src/main/java/com/udemy/hexagonal/adapters/out/InsertCustomerAdapter.java
@@ -1,4 +1,26 @@
 package com.udemy.hexagonal.adapters.out;
 
-public class InsertCustomerAdapter {
+import com.udemy.hexagonal.adapters.out.repository.CustomerRepository;
+import com.udemy.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.udemy.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.udemy.hexagonal.application.core.domain.Customer;
+import com.udemy.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void insert(Customer customer) {
+
+        CustomerEntity customerEntity = customerEntityMapper.toEntity(customer);
+        customerRepository.insert(customerEntity);
+    }
 }
---
+++ src/main/java/com/udemy/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,12 @@
+package com.udemy.hexagonal.adapters.out.repository.mapper;
+
+
+import com.udemy.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.udemy.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+
+    CustomerEntity toEntity(Customer customer);
+}
