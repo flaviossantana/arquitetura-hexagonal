8082c960e938018507c8825556f1baca6dd8157f
flavio.santana <9090flavio@gmail.com>
2026-03-13 19:17:57 -0300

    hexagonal: adicionados CostumerRepository e novas propriedades nas entidades AddressEntity e CostumerEntity

--- src/main/java/com/udemy/hexagonal/adapters/out/repository/entity/AddressEntity.java
+++ src/main/java/com/udemy/hexagonal/adapters/out/repository/entity/AddressEntity.java
@@ -1,5 +1,8 @@
 package com.udemy.hexagonal.adapters.out.repository.entity;
 
+import lombok.Data;
+
+@Data
 public class AddressEntity {
     private String street;
     private String city;
--- src/main/java/com/udemy/hexagonal/adapters/out/repository/entity/CostumerEntity.java
+++ src/main/java/com/udemy/hexagonal/adapters/out/repository/entity/CostumerEntity.java
@@ -1,8 +1,17 @@
 package com.udemy.hexagonal.adapters.out.repository.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "customer")
 public class CostumerEntity {
+
+    @Id
     private String id;
     private String name;
     private String cpf;
     private AddressEntity address;
+    private Boolean isCpfValid;
 }
