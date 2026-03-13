4346b94755a981a88c294646fc55223d253aff3b
flavio.santana <9090flavio@gmail.com>
2026-03-13 19:04:48 -0300

    hexagonal: adicionadas entidades AddressEntity e CostumerEntity e configuração para MongoDB

---
+++ src/main/java/com/udemy/hexagonal/adapters/out/repository/entity/AddressEntity.java
@@ -0,0 +1,7 @@
+package com.udemy.hexagonal.adapters.out.repository.entity;
+
+public class AddressEntity {
+    private String street;
+    private String city;
+    private String state;
+}
---
+++ src/main/java/com/udemy/hexagonal/adapters/out/repository/entity/CostumerEntity.java
@@ -0,0 +1,8 @@
+package com.udemy.hexagonal.adapters.out.repository.entity;
+
+public class CostumerEntity {
+    private String id;
+    private String name;
+    private String cpf;
+    private AddressEntity address;
+}
